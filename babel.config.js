97711c27a30504dd188ed00b2dd48f43e1a346f5
otaldogusta <gusantinho753@gmail.com>
2026-01-06 12:39:21 -0300

    fix: transform import.meta in yoga layout

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,11 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [],
+    overrides: [
+      {
+        test: /node_modules[\\/]+yoga-layout[\\/]+dist[\\/]+src[\\/]+load\.js$/,
+        plugins: [require("babel-preset-expo/build/import-meta-transform-plugin")],
+      },
+    ],
   };
 };
