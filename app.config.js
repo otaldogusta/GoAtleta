963e828c7e2ee48266697569bf90a418c60b1239
otaldogusta <gusantinho753@gmail.com>
2026-01-15 02:53:58 -0300

    chore(eas): set goatleta project id

--- app.config.js
+++ app.config.js
@@ -7,12 +7,23 @@ const appJson = require("./app.json");
 
 const base = appJson.expo ?? appJson;
 const extra = base.extra ?? {};
+const eas = extra.eas ?? {};
+const cleanedEas = { ...eas };
+delete cleanedEas.projectId;
+const easProjectId = "ac21b1cd-e0e3-495f-ba43-e262c8185ef5";
+const resolvedEas = { ...cleanedEas, projectId: easProjectId };
+
+const cleanedExtra = { ...extra };
+delete cleanedExtra.eas;
+
+cleanedExtra.eas = resolvedEas;
 
 module.exports = {
   expo: {
     ...base,
+    owner: "otaldogusta",
     extra: {
-      ...extra,
+      ...cleanedExtra,
       SUPABASE_URL:
         process.env.EXPO_PUBLIC_SUPABASE_URL ??
         process.env.SUPABASE_URL ??
