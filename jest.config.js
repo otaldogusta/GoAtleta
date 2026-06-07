af756d2d97b99bbba8469724684d4bb8872f48e3
otaldogusta <gusantinho753@gmail.com>
2026-06-06 23:18:01 -0300

    Fix Expo SDK 56 deploy dependency resolution

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.test.ts"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "^expo-modules-core$": "<rootDir>/node_modules/expo/node_modules/expo-modules-core",
-    "^expo-modules-core/(.*)$": "<rootDir>/node_modules/expo/node_modules/expo-modules-core/$1",
+    "^expo-modules-core$": "<rootDir>/node_modules/expo-modules-core",
+    "^expo-modules-core/(.*)$": "<rootDir>/node_modules/expo-modules-core/$1",
   },
 };
